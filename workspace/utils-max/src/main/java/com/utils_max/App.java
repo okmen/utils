f5b3cb88075a96fc7cd261ad0c559e9fd7063085
max_home <okmen.zy@foxmail.com>
2018-02-09 17:05:55 +0800

    sss

---
+++ workspace/utils-max/src/main/java/com/utils_max/App.java
@@ -0,0 +1,13 @@
+package com.utils_max;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
