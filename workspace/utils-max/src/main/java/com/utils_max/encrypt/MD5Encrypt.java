63f5a10d08b445dcb8f6b85993dc690cfd5a99dd
zhangyao <zhangyao@chudaokeji.com>
2018-05-25 17:22:20 +0800

    sss

--- workspace/utils-max/src/main/java/com/utils_max/encrypt/MD5Encrypt.java
+++ workspace/utils-max/src/main/java/com/utils_max/encrypt/MD5Encrypt.java
@@ -5,6 +5,7 @@ import java.security.NoSuchAlgorithmException;
 
 public class MD5Encrypt {
 	
+	
 	private static char hexChar[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9','A', 'B', 'C', 'D', 'E', 'F' };
 	/**
 	 * 将字符串 加密
